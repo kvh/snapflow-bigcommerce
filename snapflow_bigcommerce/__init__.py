677cf14d8fb59cd4840f3d169b54ed9813ab11e6
Ken Van Haren <kenvanharen@gmail.com>
2021-06-18 11:26:01 -0700

    Fix

--- snapflow_bigcommerce/__init__.py
+++ snapflow_bigcommerce/__init__.py
@@ -7,17 +7,6 @@ from .functions.import_order_products import import_order_products
 BigCommerceOrder = TypeVar("BigCommerceOrder")
 BigCommerceOrderProduct = TypeVar("BigCommerceOrderProduct")
 
-<<<<<<< Updated upstream
 module = SnapflowModule("bigcommerce", py_module_path=__file__, py_module_name=__name__)
 module.add_function(import_orders)
 module.add_function(import_order_products)
-=======
-module = SnapflowModule(
-    "example",
-    py_module_path=__file__,
-    py_module_name=__name__,
-    schemas=["schemas/{example_schema}.yml"],
-    functions=[],
-)
-module.export()
->>>>>>> Stashed changes
